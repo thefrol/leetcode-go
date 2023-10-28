02aa0eb9250b13225d7b232d82af7e5e8f2a583b
Dima Frolenko <mail@frolenkodima.ru>
2023-10-28 16:19:20 +0300

    hard: 10 Добавить бенчмарки

--- 10-regexp-matching/10-regexp_test.go
+++ 10-regexp-matching/10-regexp_test.go
@@ -36,3 +36,45 @@ func Test(t *testing.T) {
 		})
 	}
 }
+
+func Benchmark(b *testing.B) {
+	testCases := []struct {
+		pattern string
+		s       string
+		res     bool
+	}{
+		{pattern: "12313.*11", s: "12313____11", res: true},
+
+		{pattern: "aa", s: "a", res: false},
+		{pattern: "a*", s: "aa", res: true},
+		{pattern: "mis*is*ip*.", s: "mississippi", res: true},
+		// я придумал разбить миссисипи на подзадачки словно бы я тоже машина
+		{pattern: "mis*", s: "miss", res: true},
+		{pattern: "mis*", s: "missi", res: false},
+		{pattern: "mis*is*", s: "mississ", res: true},
+		{pattern: "mis*is*ip", s: "mississip", res: true},
+		{pattern: "mis*is*ip*", s: "mississipp", res: true},
+		//{pattern: "miss*s.s.sasdlnaweiwen*.**", s: "missis.sdfisdfalksd**", res: false},
+		/// ага! значит не работают точки
+		{pattern: "...", s: "abc", res: true}, /// чиню
+		{pattern: "..c", s: "abc", res: true}, // не работает точка в конце
+		// неправильное условие стояло
+		//
+		// господи, спасибо тестам
+	}
+	for i := 0; i < b.N; i++ {
+		for _, v := range testCases {
+			isMatch(v.s, v.pattern)
+		}
+
+	}
+}
+
+// без кеша
+//
+//cpu: Intel(R) Core(TM) i5-6300U CPU @ 2.40GHz
+//Benchmark-4   	  229239	      5371 ns/op	    1616 B/op	      64 allocs/op
+
+// сделал проверку продстрок
+// cpu: Intel(R) Core(TM) i5-6300U CPU @ 2.40GHz
+// Benchmark-4   	  200332	      5660 ns/op	    1200 B/op	      51 allocs/op
