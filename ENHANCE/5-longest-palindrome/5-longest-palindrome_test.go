25830b72e38772458f615bb3cdd4402d701ffacd
Dima Frolenko <mail@frolenkodima.ru>
2023-10-29 03:20:35 +0300

    5 сдан! можно ускорить

--- ENHANCE/5-longest-palindrome/5-longest-palindrome_test.go
+++ ENHANCE/5-longest-palindrome/5-longest-palindrome_test.go
@@ -23,60 +23,62 @@ import (
 
 // V2 стоит поменять принцип работы КЕША. Он должен как можно быстрее говорить, что тут палиндрома нет
 // Значит если кеш==тру, тут никаких палиндромов
+
+// V3 использую оба кэша
+
+// V4 можем не пользоваться этим странным шагающим окном, по сути мы все равно проверяем
+// все пары начала и конца. Сделаем все одним большим циклом, и просто последим, чтобы
+// как можно быстрее все это закончилось)))
+
+// ИДЕЯ V5, мне кажется более быстрее будет растить палиндромы, что думаешь? от каждого символа и и от каждых двух
+
 var cache [][]bool
 var nopal [][]bool
-var longestS []byte
 
 func longestPalindrome(s string) string {
 	cache = make([][]bool, len(s))
 	nopal = make([][]bool, len(s)) // возможно сделать и сплошной массив, главное потом с индексами не напутать)
-	for i := 0; i < len(s); i++ {
+	for i := 0; i < len(s); i++ {  // плюс один опять из-за проблем с левой границей
 		cache[i] = make([]bool, len(s))
 		nopal[i] = make([]bool, len(s))
 	}
-	return string(longest([]byte(s), 0, len(s)-1))
-}
 
-func longest(source []byte, left, right int) []byte {
-	//print("longest", left, right)
-	if len(source) < right-left {
-		return longestS
+	var longest []byte
+	source := []byte(s)
+	for l := 0; l < len(s); l++ { // можно потом -1 сделать
+		if len(s)-l < len(longest) {
+			break
+		}
+		for r := l; r < len(s); r++ {
+			if r-l+1 <= len(longest) { // плюс один, потому что, когда l=r они указывают на один символ, по сути длинна сейчас 1
+				continue // ранний сброс, если уже что-то да нашли
+			}
+			if isPalindrome(source, l, r) {
+				longest = source[l : r+1]
+			}
+		}
 	}
-	if left == right {
-		return source[left : right+1] // +1 потому что правая граница всегда не включается, и мы хотим ее включить
-	}
-	if isPalindrome(source, left, right) {
-		return source[left : right+1]
-	}
-	leftS := longest(source, left+1, right)
-	rightS := longest(source, left, right-1)
-	if len(leftS) > len(rightS) {
-		return leftS
-	} else {
-		return rightS
-	}
+	return string(longest)
 }
 
 func isPalindrome(source []byte, left, right int) bool {
+	// сначала проверим кэш
 	if nopal[left][right] == true {
 		return false
 	}
 	if cache[left][right] == true {
 		return true
 	}
+	// теперь уже как обычно
 	if len(source) == 0 {
 		return false
 	}
 	if len(source) == 1 {
-		if len(longestS) < right-left {
-			longestS = source[left : right+1]
-		}
 		return true
 	}
-	// if val, ok := cache[source]; ok {
-	// 	return val
-	// }
+
 	for l, r := left, right; r-l >= 1; l, r = l+1, r-1 {
+		// сначала проверяем кэш
 		if cache[l][r] == true {
 			break // чтобы записать в кеш
 		}
@@ -86,10 +88,12 @@ func isPalindrome(source []byte, left, right int) bool {
 			}
 			return false // чтобы записать в кеш
 		}
+
 		if source[l] != source[r] {
 			for l, r := l, r; l == left && r == right; l, r = l-1, r+1 {
 				nopal[l][r] = true // возможно если у нас есть более глобальный палиндром, то можно как бы с более мелкими не заморачиваться, которые внутри большого
 			}
+
 			return false
 		}
 	}
@@ -103,16 +107,17 @@ func TestTooLong(t *testing.T) {
 	longestPalindrome("babaddtattarrattatddetartrateedredividerb")
 }
 
-func Test(t *testing.T) {
+func TestTable(t *testing.T) {
 	testCases := []struct {
 		input      string
 		palindrome string
 	}{
+		{input: "a", palindrome: "a"}, // 114 testcase
 		{input: "badad", palindrome: "ada"},
 		{input: "cbbd", palindrome: "bb"},
 		{input: "sisiooppoosos", palindrome: "ooppoo"},
 		{input: "sisiooppoososaaddaaccaaddaa", palindrome: "aaddaaccaaddaa"},
-		//{input: "babaddtattarrattatddetartrateedredividerb", palindrome: "123"},
+		{input: "babaddtattarrattatddetartrateedredividerb", palindrome: "ddtattarrattatdd"},
 	}
 	for _, tC := range testCases {
 		t.Run(tC.input, func(t *testing.T) {
